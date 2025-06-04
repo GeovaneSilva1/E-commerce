708d618b71283beccfda58d09ae0d920be52e6ad
Geovane Brito <geovanealu@gmail.com>
2025-06-03 23:25:47 -0300

    SMS-1 - Feito crud completo da tabela clientes.

---
+++ LojaVirtual.ProductApi/Models/IClienteRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace LojaVirtual.ProductApi.Models
+{
+    public interface IClienteRepository
+    {
+         void Add(Cliente cliente);
+         
+        List<Cliente> Get();
+
+        Cliente GetById(int id);
+
+        bool ExistById(int id);
+
+        Cliente Update(Cliente cliente);
+
+        Cliente DeleteById(int id);
+    }
+}
