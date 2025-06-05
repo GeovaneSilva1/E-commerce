6aa5506bee7a71bd8ead7c418ea3fea4fea96e36
Geovane de Nazaré da Silva Brito <geovane.brito@benner.com.br>
2025-06-05 11:05:56 -0300

    SMS-4 - criado repository de venda

---
+++ LojaVirtual.ProductApi/Infraestrutura/VendaRepository.cs
@@ -0,0 +1,21 @@
+﻿using LojaVirtual.ProductApi.Context;
+using LojaVirtual.ProductApi.Models;
+
+namespace LojaVirtual.ProductApi.Infraestrutura
+{
+    public class VendaRepository : IVendaRepository
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public VendaRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public void Add(Venda venda)
+        {
+            _appDbContext.Vendas.Add(venda);
+            _appDbContext.SaveChanges();
+        }
+    }
+}
