aeb23cba5bad9cdbfa64518c07c67b0469ec384e
Geovane S <geovanealu@gmail.com>
2025-07-26 23:21:18 -0300

    criada página para cadastro de categorias

---
+++ LojaVirtual.Web/Controllers/CategoriasController.cs
@@ -0,0 +1,24 @@
+﻿using LojaVirtual.Web.Models;
+using LojaVirtual.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Web.Controllers
+{
+    public class CategoriasController : Controller
+    {
+        private readonly ICategoriaService _categoriaService;
+
+        public CategoriasController(ICategoriaService categoriaService)
+        {
+            _categoriaService = categoriaService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProdutoViewModel>>> Index()
+        {
+            var categorias = await _categoriaService.ObterCategoriasAsync();
+
+            return View(categorias);
+        }
+    }
+}
