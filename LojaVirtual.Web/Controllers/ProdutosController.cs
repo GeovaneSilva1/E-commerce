a62d4cbdb3fe81d59c34052fcba55cd4dd804294
Geovane Brito <geovanealu@gmail.com>
2025-08-11 00:11:06 -0300

    carregando as imagens no modal sem fechar a tela

--- LojaVirtual.Web/Controllers/ProdutosController.cs
+++ LojaVirtual.Web/Controllers/ProdutosController.cs
@@ -1,9 +1,14 @@
 ﻿using LojaVirtual.Web.Models;
 using LojaVirtual.Web.Models.Compostas;
 using LojaVirtual.Web.Services.Interfaces;
+using Microsoft.AspNetCore.Components.Endpoints.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using System.Diagnostics;
+using System.Reflection;
+using static System.Net.Mime.MediaTypeNames;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace LojaVirtual.Web.Controllers
@@ -140,33 +145,48 @@ namespace LojaVirtual.Web.Controllers
         [HttpPost(), ActionName("AdicionarImagemProduto")]
         public async Task<IActionResult> AdicionarImagemProduto([FromForm] ProdutoImagensViewModel produtoImagensViewModel)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                try
-                {
-                    if (produtoImagensViewModel.Files is null)
-                    {
-                        TempData["MensagemErro"] = "Erro ao adicionar imagens.";
-                        return RedirectToAction(nameof(Index));
-                    }
-                    if (produtoImagensViewModel.HandleProduto <= 0)
-                    {
-                        TempData["MensagemErro"] = "Erro ao adicionar imagens. Produto não encontrado";
-                        return RedirectToAction(nameof(Index));
-                    }
-
-                    var imagensAdicionadas = await _ImagemProdutoService.UploadImagensAsync(produtoImagensViewModel.Files, produtoImagensViewModel.HandleProduto);
-                    TempData["MensagemSucesso"] = "Imagens adicionadas com sucesso!";
-                }
-                catch (Exception ex)
-                {
-                    TempData["MensagemErro"] = ex.Message;
-                    return RedirectToAction(nameof(Index));
-                }
+                return Json(new { success = false, message = "Nenhuma imagem selecionada." });
             }
 
-            return RedirectToAction(nameof(Index));
-            
+            try
+            {
+                var imagensAdicionadas = await _ImagemProdutoService.UploadImagensAsync(produtoImagensViewModel.Files, produtoImagensViewModel.HandleProduto);
+                string htmlAtualizado = await RenderPartialViewToString("_ListaImagens", imagensAdicionadas);
+
+                return Json(new { success = true, html = htmlAtualizado, message = "Imagem enviada com sucesso!" });
+            }
+            catch(Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+
+        private async Task<string> RenderPartialViewToString(string htmlName, IEnumerable<ImagemProdutoViewModel> imagensAdicionadas)
+        {
+            var viewEngine = HttpContext.RequestServices.GetService(typeof(ICompositeViewEngine)) as ICompositeViewEngine;
+            var viewResult = viewEngine.FindView(ControllerContext, htmlName, false);
+
+            if (!viewResult.Success)
+                throw new FileNotFoundException($"View '{htmlName}' não encontrada.");
+
+            ViewData.Model = imagensAdicionadas;
+
+            using (var sw = new StringWriter())
+            {
+                var viewContext = new ViewContext(
+                    ControllerContext,
+                    viewResult.View,
+                    ViewData,
+                    TempData,
+                    sw,
+                    new HtmlHelperOptions()
+                );
+
+                await viewResult.View.RenderAsync(viewContext);
+                return sw.ToString();
+            }
         }
     }
 }
