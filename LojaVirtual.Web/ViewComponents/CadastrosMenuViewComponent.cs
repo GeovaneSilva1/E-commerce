9ed0878e30cf6b6e47472c202e3d03abf00067d1
Geovane Brito <geovanealu@gmail.com>
2025-09-01 11:32:02 -0300

    ajustada tela inicial e separado o catalogo de produtos por categoria.

---
+++ LojaVirtual.Web/ViewComponents/CadastrosMenuViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Web.ViewComponents
+{
+    public class CadastrosMenuViewComponent : ViewComponent
+    {
+        public CadastrosMenuViewComponent()
+        {
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View("Default");
+        }
+    }
+}
