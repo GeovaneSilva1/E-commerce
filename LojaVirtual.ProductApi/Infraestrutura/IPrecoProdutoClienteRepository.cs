c992716a5fee2272803bdf6b6a612a73ac247099
Geovane de Nazaré da Silva Brito <geovane.brito@benner.com.br>
2025-09-15 13:59:57 -0300

    removido microservice productAPI

--- LojaVirtual.ProductApi/Infraestrutura/IPrecoProdutoClienteRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using LojaVirtual.ProductApi.Models;
-
-namespace LojaVirtual.ProductApi.Infraestrutura
-{
-    public interface IPrecoProdutoClienteRepository
-    {
-        Task Add(PrecoProdutoCliente precoProdutoCliente);
-    }
-}
